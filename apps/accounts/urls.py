c6436ec2ce6c22f8d680cdf7f6b871c96bc8e139
inu9431 <inu9431@naver.com>
2025-07-22 14:45:11 +0900

    account/testcode

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-# from .views import AccountDeleteAPIView, AccountListCreateApiView
+from .views import AccountDeleteAPIView, AccountListCreateApiView
 
 app_name = "accounts"
 
 urlpatterns = [
-    # path(
-    #      "list-create/", AccountListCreateApiView.as_view(), name="account-list-create"
-    # ),
-    # path("delete/<int:pk>/", AccountDeleteAPIView.as_view(), name="account-delete"),
+     path(
+          "list-create/", AccountListCreateApiView.as_view(), name="account-list-create"
+     ),
+     path("delete/<int:pk>/", AccountDeleteAPIView.as_view(), name="account-delete"),
 ]
