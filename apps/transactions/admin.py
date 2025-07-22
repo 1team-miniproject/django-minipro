1c8e358d4e3bb7e5c88ecd7f4c95101ee8d4375c
daeun0608 <glace0608@naver.com>
2025-07-22 18:37:07 +0900

    transactions test code

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Transaction
 
+class AmountRangeFilter(admin.SimpleListFilter):
+    title = "Amount Range"
+    parameter_name = "amount"
+    def lookups(self, request, model_admin):
+        return (
+        ('0~10000', '1만 -'),
+        ('10000~100000', '1만 ~ 10만'),
+        ('100000~1000000', '10만 ~ 100만'),
+        ('1000000+', '100만 +')
+        )
+
+    def queryset(self, request, queryset):
+        if self.value():
+            if self.value() == '1000000+':
+                return queryset.filter(amount__gte=1000000)
+            else:
+                min,max=map(int,self.value().split('~'))
+                return queryset.filter(amount__gte=min, amount__lt=max)
+        return queryset
+
+
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    pass
+    list_display=("id", "account_id", "amount", "io_type", "created_at")
+    list_filter = ("io_type", AmountRangeFilter)
--- apps/transactions/serializers.py
+++ apps/transactions/serializers.py
@@ -5,5 +5,5 @@ from .models import Transaction
 class TransactionSerializer(ModelSerializer):
     balance_after_transaction = IntegerField(default=0)
     class Meta:
-        model=Transaction
+        model= Transaction
         fields='__all__'
