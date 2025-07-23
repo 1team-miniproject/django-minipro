e77a35af84c14978b771a2621e197743c9fe189e
inu9431 <inu9431@naver.com>
2025-07-23 15:45:14 +0900

    settings.base 수정

--- apps/config/settings/base.py
+++ apps/config/settings/base.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),   # 예시
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),   # 예시
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKEN': True,
     'BLACKLIST_AFTER_ROTATION': True,
