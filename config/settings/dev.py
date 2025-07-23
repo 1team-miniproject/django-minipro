2bfea7c7294500283d3d452ff9498f1bce9c3121
Syapa723 <birdragon2@gmail.com>
2025-07-23 11:38:44 +0900

    배포 테스트

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,8 +4,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",  # 1-2에서 생성한 데이터베이스 이름
-        "USER": "choi",  # 1-1에서 생성한 사용자 이름
-        "PASSWORD": "12345",  # 1-1에서 설정한 비밀번호
+        "USER": "syapa",  # 1-1에서 생성한 사용자 이름
+        "PASSWORD": "418770",  # 1-1에서 설정한 비밀번호
         "HOST": "localhost",  # 내 컴퓨터에서 실행 중이므로 'localhost'
         "PORT": "5432",  # PostgreSQL 기본 포트
     }
