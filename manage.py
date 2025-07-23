c16b358b29ce6c97f25ba6c5e83d5c35c1c2c32a
inu9431 <inu9431@naver.com>
2025-07-23 15:40:36 +0900

    manage.py 수정

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(BASE_DIR, 'apps'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
     try:
         from django.core.management import execute_from_command_line
